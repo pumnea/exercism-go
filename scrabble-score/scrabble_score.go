81649bda3ed982b0d4deaf4fd8fdc0f1167aeadf
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-03-14 16:23:50 +0200

    add: Score function body to calculate Scrabble scroe of a word
    add: Package and function level comments.

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,34 @@
+// Package scrabble provides utilities to calculate Scrabble scores for words.
+package scrabble
+
+import (
+	"strings"
+)
+
+// Score returns the Scrabble score of a word based on its letter values.
+func Score(word string) int {
+	if len(word) == 0 {
+		return 0
+	}
+	score := 0
+	word = strings.ToUpper(word)
+	for _, l := range word {
+		switch l {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
+	}
+	return score
+}
