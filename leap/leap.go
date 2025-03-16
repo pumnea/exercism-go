cc072b5b2f3542b4f6db56325890088eafef307e
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-03-16 13:08:32 +0200

    add: Logic to check if input is a leap year
    add: IsLeapYear returns true if input is a leap year.
    add: Package and functions comments added.

---
+++ leap/leap.go
@@ -0,0 +1,13 @@
+// Package leap provides utilities to check if a given year is leap.
+package leap
+
+// IsLeapYear returns true if input is a leap year.
+// Divisible by 4 and either not by 100 or by 400.
+func IsLeapYear(year int) bool {
+	switch {
+	case year%4 == 0 && (year%100 != 0 || year%400 == 0):
+		return true
+	default:
+		return false
+	}
+}
